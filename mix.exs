47f6f43e043e7d2659ffc803c4f0f377bd37a77f
Antonis Kalou <kalouantonis@protonmail.com>
2018-03-09 22:09:26 +0100

    Rename app from portaudio to ex_portaudio
    This was causing issues for loading the dependency under _build as
    it is referenced as :ex_portaudio but it is compiled under
    _build/../ex_portaudio/ebin/portaudio.app

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PortAudio.MixProject do
 
   def project do
     [
-      app: :portaudio,
+      app: :ex_portaudio,
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
